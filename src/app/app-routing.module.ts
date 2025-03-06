3492d5228959f9d628fe5f38619426faf4a0204e
Панов Даниил <kartoshkavkusnay@gmail.com>
2025-03-06 11:34:26 +0200

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoinListComponent } from './components/coin-list/coin-list.component';
 import { CoinChartComponent } from './components/coin-chart/coin-chart.component';
+import { PlatformHelper } from  '@natec/mef-dev-platform-connector';
+
 
 const routes: Routes = [
   { path: '', component: CoinListComponent },  
--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,13 +1,13 @@
 export const PLUGIN_VERSION = {
     "dirty": true,
-    "raw": "073a370-dirty",
-    "hash": "073a370",
+    "raw": "5484e4a-dirty",
+    "hash": "5484e4a",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "073a370-dirty",
+    "suffix": "5484e4a-dirty",
     "semverString": null,
     "name": "crypto-tracker",
-    "version": "1.1.1",
+    "version": "1.1.2",
     "main": "main.js"
 };
